28ab04b6ee709a72cc1f66f7f95ae6fe19fe045a
Maksim Bogorad <115224623+spcsmm100@users.noreply.github.com>
2022-10-13 19:07:32 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,4 +1,5 @@
 #На вход подается строка, состоящая из одного числа. Напишите программу, которая удваивает его.
+# Input 646 Output 1292
 # try:
 #	str1 = int(input("Enter the number: "))
 # 	print(str1 * 2)	
@@ -6,6 +7,7 @@
 # 	print("This is not a number!")
 
 #Вводится число. Вывести его квадрат.
+# Input 23 Output 529
 # def square(num):
 # 	return num*num
 # try:
@@ -16,16 +18,19 @@
 
 #Вводятся часы, минуты и секунды. Вывести, сколько секунд прошло с полуночи.
 #Вывести, какая часть суток прошла (число от 0 до 1).
+#Input 19 54 12 Output 0.8293
 # try:
 # 	h = int(input("Enter the hours: "))
 # 	m = int(input("Enter the minutes: "))
 # 	s = int(input("Enter the seconds: "))
-# 	print("\nAmount of seconds: ", h*3600 + m*60 + s)
-# 	print("Day part: %.4f" %((h*3600 + m*60 + s) / (24*3600)))
+# 	t = h*3600 + m*60 + s
+# 	print("\nAmount of seconds: ", t)
+# 	print("Day part: ", round((t / (24*3600)), 4))
 # except:
 # 	print("You have entered bad!")
 
 #Вводится число. Вывести, оканчивается ли оно на цифру 7, не используя приведение к строке и операции над строками
+# Input 345437 Output TRUE
 # try:
 # 	num = int(input("Enter the number: "))
 # 	print(num % 10 == 7)
@@ -33,6 +38,9 @@
 # 	print("This is not a number!") 
 
 #Вводятся коэффициенты уравнения ax2+bx+c=0. Вывести его корни(не забыть проверить, что a не равно 0)
+#Input 0, 1, 2 Output -2.0
+#Input 6, 16, 26 Output -3.8054 1.1387
+#Input -4 28 -49 Output 3.5
 
 # def discrim(a, b, c):
 # 	return b*b - 4*a*c
@@ -45,7 +53,7 @@
 # 	print("You have entered not a numbers!")
 
 # if a == 0:
-# 	print("Not a quadratic equation!")
+# 	print("Root is: ", -c/b)
 # 	exit()
 
 # d = discrim(a, b, c)
@@ -57,23 +65,23 @@
 # 	exit()
 
 # d = d**0.5
-# print("First root is: %.4f" %((-b-d)/(2*a)))
-# print("Second root is: %.4f" %((-b+d)/(2*a)))
+# print("First root is: ", round(((-b-d)/(2*a)), 4))
+# print("Second root is: ", round(((-b+d)/(2*a)), 4))
 
 
 #Вводятся три числа. Вывести максимум из них.
-
+#Input 4 14 5 Output 14
 # try:
 # 	arr = [0, 0, 0]
 # 	for i in range(3):
 # 		arr[i] = float(input("Enter the number: "))
-# 	print("Max is: %.0f" % max(arr))
+# 	print("Max is: ", round(max(arr), 0))
 # except:
 # 	print("You have entered not a numbers!")
 
 
 #Вводится число. Вывести среднее арифметическое (с точностью до двух знаков после запятой) тех чисел в диапазоне от единицы до введённого числа, которые делятся 5 или являются четными.
-
+#Input 23 Output 11.69
 # try:
 # 	num = int(input("Enter the number: "))
 # 	summ = 0
@@ -83,6 +91,6 @@
 # 			summ += i
 # 			n += 1
 # 	n -= 1
-# 	print('Sr ar is: %.2f' % (summ/n))
+# 	print('Sr ar is: ', round((summ/n), 2))
 # except:
 # 	print("You have entered not a number!")
